ac68cfb7a15535c69ea1750d25752eb38cfbc0a8
Shiba <44804845+DeepDoge@users.noreply.github.com>
2023-10-17 11:09:13 +0000

    ...

--- packages/master-ts/extra/awaited.ts
+++ packages/master-ts/extra/awaited.ts
@@ -1,13 +1,14 @@
 import type { Signal, SignalOrValueOrFn } from "master-ts/core.ts"
 import { derive, isSignalOrFn, signal, signalFrom } from "master-ts/core.ts"
+import { flatten } from "./"
 
 export let awaited: {
 	<T>(promise: SignalOrValueOrFn<Promise<T>>): Signal<T | null>
 	<T, U>(promise: SignalOrValueOrFn<Promise<T>>, until: U): Signal<T | U>
-} = (promise: SignalOrValueOrFn<Promise<unknown>>, until: unknown = null) => {
+} = (promise: SignalOrValueOrFn<Promise<unknown>>, until: unknown = null): Signal<unknown> => {
 	if (isSignalOrFn(promise)) {
 		const promiseSignal = signalFrom(promise)
-		return derive(() => awaited(promiseSignal.ref, until), [promiseSignal])
+		return flatten(derive(() => awaited(promiseSignal.ref, until), [promiseSignal]))
 	}
 	const promiseSignal = signal(until)
 	promise.then((value) => (promiseSignal.ref = value))
