584454097a1ce6440dea1a6a445dcb415dba1aec
Shiba <44804845+DeepDoge@users.noreply.github.com>
2023-11-03 14:19:46 +0000

    ...

---
+++ apps/todo/src/calculator.ts
@@ -0,0 +1,116 @@
+import { css, sheet, signal, tags } from "master-ts"
+
+const { div, input, button, style } = tags
+
+export function Calculator() {
+    const value = signal("")
+    const sanitizedValue = () => value.ref.split("").filter(allowed.has.bind(allowed)).join("")
+    const result = () => String(eval(sanitizedValue()))
+
+    const allowed = new Set("0123456789+-*/().")
+
+    function isNumber(value: string) {
+        return !isNaN(Number(value))
+    }
+
+    return div({ class: "calculator" }, [
+        input({ class: "input", type: "text", "bind:value": value }),
+        div({ class: "result" }, [() => (isNumber(result()) ? result() : "")]),
+
+        div({ class: "buttons" }, [
+            button({ "on:click": () => (value.ref = ""), "style:grid-area": "clear" }, ["Clear"]),
+            button({ "on:click": () => (value.ref += "1"), "style:grid-area": "one" }, ["1"]),
+            button({ "on:click": () => (value.ref += "2"), "style:grid-area": "two" }, ["2"]),
+            button({ "on:click": () => (value.ref += "3"), "style:grid-area": "three" }, ["3"]),
+            button({ "on:click": () => (value.ref += "4"), "style:grid-area": "four" }, ["4"]),
+            button({ "on:click": () => (value.ref += "5"), "style:grid-area": "five" }, ["5"]),
+            button({ "on:click": () => (value.ref += "6"), "style:grid-area": "six" }, ["6"]),
+            button({ "on:click": () => (value.ref += "7"), "style:grid-area": "seven" }, ["7"]),
+            button({ "on:click": () => (value.ref += "8"), "style:grid-area": "eight" }, ["8"]),
+            button({ "on:click": () => (value.ref += "9"), "style:grid-area": "nine" }, ["9"]),
+            button({ "on:click": () => (value.ref += "0"), "style:grid-area": "zero" }, ["0"]),
+            button({ "on:click": () => (value.ref += "+"), "style:grid-area": "add" }, ["+"]),
+            button({ "on:click": () => (value.ref += "-"), "style:grid-area": "subtract" }, ["-"]),
+            button({ "on:click": () => (value.ref += "*"), "style:grid-area": "multiply" }, ["*"]),
+            button({ "on:click": () => (value.ref += "/"), "style:grid-area": "divide" }, ["/"]),
+            button({ "on:click": () => (value.ref += "."), "style:grid-area": "decimal" }, ["."]),
+            button({ "on:click": () => (value.ref += "("), "style:grid-area": "open" }, ["("]),
+            button({ "on:click": () => (value.ref += ")"), "style:grid-area": "close" }, [")"])
+        ]),
+        style([
+            css`
+                @scope {
+                    :scope {
+                        display: grid;
+                        grid-template-columns: minmax(0, 10em);
+                        place-content: center;
+
+                        gap: 0.5em;
+
+                        font-family: sans-serif;
+                        font-size: 2rem;
+                    }
+
+                    .input {
+                        all: unset;
+                        display: grid;
+                        text-align: right;
+                        font: inherit;
+                        background-color: hsl(210, 100%, 50%);
+                        color: white;
+                        padding: 0.25em 0.5em;
+                        border-radius: 0.25em;
+                    }
+
+                    .result {
+                        &:not(:empty)::before {
+                            content: "= ";
+                        }
+                        font-size: 0.5em;
+                        opacity: 0.75;
+                        height: 1.5ch;
+                        justify-self: end;
+                    }
+
+                    .buttons {
+                        display: grid;
+                        grid-template-columns: repeat(4, 1fr);
+                        grid-template-rows: repeat(5, 1fr);
+                        gap: 0.5em;
+
+                        grid-template-areas:
+                            "clear  clear   open    close"
+                            "seven  eight   nine    divide"
+                            "four   five    six     multiply"
+                            "one    two     three   subtract"
+                            "zero   zero    decimal add";
+
+                        & button {
+                            all: unset;
+                            display: grid;
+                            place-items: center;
+                            background-color: hsl(210, 100%, 50%);
+                            color: white;
+                            border-radius: 0.25em;
+                            &:hover {
+                                background-color: hsl(210, 100%, 40%);
+                            }
+                        }
+                    }
+                }
+            `
+        ])
+    ])
+}
+
+document.adoptedStyleSheets.push(
+    sheet(css`
+        *,
+        *::before,
+        *::after {
+            box-sizing: border-box;
+        }
+    `)
+)
+
+document.body.append(Calculator())
