27c446a613797f45a4a4d8e85321b6e5afbc3148
Shiba <44804845+DeepDoge@users.noreply.github.com>
2023-12-24 19:43:50 +0000

    :/

--- packages/master-ts/jsx-runtime.ts
+++ packages/master-ts/jsx-runtime.ts
@@ -8,13 +8,14 @@ export namespace JSX {
 
 type ChildrenProp = { children?: Template.Member[] | Template.Member }
 type Factory = { (props: Template.Attributes<Element> & ChildrenProp): unknown }
+// type FactoryProps<T extends Factory> = ChildrenProp & { $: Parameters<T> } // TypeScript doesn't care about this :/
 
 export function jsx<K extends keyof HTMLElementTagNameMap, TInputType extends HTMLInputElement["type"]>(
     name: K,
     props: Template.Attributes<HTMLElementTagNameMap[K], TInputType> & ChildrenProp
 ): HTMLElementTagNameMap[K]
 export function jsx(name: string, props: Template.Attributes<HTMLElement> & ChildrenProp): Element
-export function jsx(factory: Factory, props: ChildrenProp): ReturnType<Factory>
+// export function jsx<T extends Factory>(factory: T, props: FactoryProps<T>): ReturnType<Factory> // TypeScript doesn't care about this :/
 export function jsx(nameOrFactory: string | Factory, props: Template.Attributes<Element> & ChildrenProp): unknown {
     if (typeof nameOrFactory === "function") return nameOrFactory(props)
 
